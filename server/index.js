d9b109478d35ecae5da7bde063b4ddc8e1ee1a26
mahareact1 <mahamanivai@gmail.com>
2026-02-02 15:14:40 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import dotenv from 'dotenv'
+//import route from './routes/'
+
+const app=express();
+app.use(bodyParser.json());
+app.use(cors());
+dotenv.config();
+
+const PORT =process.env.PORT || 8009;
+const MONGOURL  =process.env.MONGO_URL;
+
+mongoose.connect(MONGOURL).then(()=>{
+    console.log("mongodb conneection success...")
+    app.listen(PORT,()=>{
+    console.log(`server port listening at ${PORT}...`)
+    })
+}).catch((err)=>{
+    console.log("Error",err.toString())
+})
